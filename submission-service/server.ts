4613967fe563224298bb2f8ccfcce994ce226d88
Tushar <tusharbhatt0135@gmail.com>
2026-07-10 11:52:42 +0530

    Add express-rate-limit middleware to submission service for request rate limiting; update package.json and package-lock.json to include new dependency.

--- submission-service/server.ts
+++ submission-service/server.ts
@@ -2,10 +2,21 @@ import express from "express";
 import { logger } from "./src/config/logger.config";
 import { connectDB } from "./src/config/db.config";
 import { v1Router } from "./src/routers/index.router";
+import { rateLimit } from "express-rate-limit";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000,
+	max: 10, 
+	message: "Too many requests, please try again later.",
+	standardHeaders: "draft-8",
+	legacyHeaders: false,
+});
+
+app.use(limiter);
 
 // ROUTES
 app.use("/api/v1", v1Router);

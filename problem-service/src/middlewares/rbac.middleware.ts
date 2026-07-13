602ddb761caa551920ee2009e728bc25b1b462b5
Tushar <tusharbhatt0135@gmail.com>
2026-07-13 12:17:56 +0530

    Refactor RBAC middleware to improve error handling; replace direct response with error throwing for unauthenticated users and enhance unauthorized message clarity.

--- problem-service/src/middlewares/rbac.middleware.ts
+++ problem-service/src/middlewares/rbac.middleware.ts
@@ -16,11 +16,7 @@ export async function rbacMiddlewWare(
 	try {
 		const token = await req.cookies.leetcode_user;
 		if (!token) {
-			res.status(401).json({
-				message: "User not authenticated",
-				success: false,
-			});
-			return;
+			throw new Error("Unauthenticated, token not found.");
 		}
 
 		const verifyToken = jwt.verify(token, authConfig.JWT_PUBIC_KEY!, {
@@ -30,14 +26,14 @@ export async function rbacMiddlewWare(
 
 		if (role !== UserRole.ADMIN && role !== UserRole.PROBLEM_SETTER) {
 			return res.status(403).json({
-				message: "Unauthorized, only Admin and Problem setter can access it",
+				message: "Unauthorized, only Admin and Problem setter can access it.",
 				success: false,
 			});
 		}
 		next();
 	} catch (error) {
 		return res.status(401).json({
-			message: "Unauthenticated",
+			message: error instanceof Error ? error.message : "Unauthenticated",
 			success: false,
 		});
 	}
